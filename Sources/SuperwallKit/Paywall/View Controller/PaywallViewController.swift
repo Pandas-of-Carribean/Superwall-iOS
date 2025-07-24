5a636190dd606be3c9d28a300e3a113aa9e1d124
HongPingRao <1836619909@qq.com>
2025-07-24 17:38:46 +0800

    reset： 还原

--- Sources/SuperwallKit/Paywall/View Controller/PaywallViewController.swift
+++ Sources/SuperwallKit/Paywall/View Controller/PaywallViewController.swift
@@ -1037,15 +1037,6 @@ extension PaywallViewController {
 
         Superwall.shared.dependencyContainer.delegateAdapter.didDismissPaywall(withInfo: info)
     }
-
-    override open func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if Superwall.shared.dependencyContainer.deviceHelper.interfaceStyleOverride == nil {
-            if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
-                reloadWebView()
-            }
-        }
-    }
 }
 
 #if !os(visionOS)
